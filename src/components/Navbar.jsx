edda1694baae31b1a9f57168d48d9891d3d978c4
Jaimin Malaviya <Jaiminmalviya56.jm@gmail.com>
2024-01-13 15:49:38 +0530

    navbar css changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import Link from 'next/link'
 
 const Navbar = () => {
    return (
-      <nav className="container flex justify-between items-center sm:py-8 py-4 sm:mb-2 mb-0 backdrop-blur">
-         <Link href="/">
+      <nav className="container flex justify-between items-center sm:py-4 py-1 mb-6 lg:mb-8 sm:pl-0 pl-2 m-auto border-b border-gray-400">
+         <Link
+            href="/"
+            className="outline-0"
+         >
             <h1 className="md:text-5xl text-4xl font-bold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text md:leading-[64px] leading-[48px]">
                Dashboard
             </h1>
